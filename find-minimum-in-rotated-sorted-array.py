547d048548ec37931251d18d80f4df6aeb82a0eb
UniqueLJH <jiahangli@hustunique.com>
2015-09-29 22:41:44 +0800

    find-minimum-in-rotated-sorted-array.py

---
+++ find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,9 @@
+ass Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        return nums[0]
+        
